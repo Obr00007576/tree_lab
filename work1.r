258b99929d4015f901f3b76ebbac09cf5ac99ee6
Obr00007576 <51303014+Obr00007576@users.noreply.github.com>
2023-04-19 19:07:24 +0800

    0.0.1

--- work1.r
+++ work1.r
@@ -1,14 +1,16 @@
 library(mlbench)
+data(Glass)
 library(rpart)
 library(rpart.plot)
+set.seed(123)
 
-data("Glass")
-train_index <- sample(1:nrow(Glass), round(nrow(Glass) * 0.8))
-train_data <- Glass[train_index, ]
-test_data <- Glass[-train_index, ]
+tree <- rpart(Type ~ ., data = Glass)
 
-tree <- rpart(Type ~ ., data = train_data, method = "class")
+rpart.plot(tree, extra = 101, cex = 0.7)
 
-png(filename = "glass_tree.png")
-rpart.plot(tree, extra = 1, cex = 0.7)
-dev.off()
+cv <- rpart.control(cp = 0.01, xval = 10)
+cv_tree <- prune(tree, cp = tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"])
+
+rpart.plot(cv_tree, extra = 101, cex = 0.7)
+
+print(cv)
