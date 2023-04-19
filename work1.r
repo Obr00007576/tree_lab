d962cc2cc62a9d2007ca8c618dcaad61d6b6c9b7
Obr00007576 <51303014+Obr00007576@users.noreply.github.com>
2023-04-19 19:11:45 +0800

    0.0.2

--- work1.r
+++ work1.r
@@ -6,11 +6,15 @@ set.seed(123)
 
 tree <- rpart(Type ~ ., data = Glass)
 
-rpart.plot(tree, extra = 101, cex = 0.7)
+#png(file = "glass_tree.png")
+#rpart.plot(tree, extra = 101, cex = 0.7)
+#dev.off()
 
 cv <- rpart.control(cp = 0.01, xval = 10)
 cv_tree <- prune(tree, cp = tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"])
 
+png(file = "pruned_glass_tree.png")
 rpart.plot(cv_tree, extra = 101, cex = 0.7)
+dev.off()
 
 print(cv)
